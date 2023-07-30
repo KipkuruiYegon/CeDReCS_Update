d7db9fe8d97dc0465fe0021883a0c8eb713e9d1d
KipkuruiYegon <kevinkipkurui1@gmail.com>
2023-07-30 23:29:41 +0300

    finished the project up for hosting

--- mailscript.js
+++ mailscript.js
@@ -1,23 +1,57 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var btn = document.getElementById('btn');
-    btn.addEventListener('click', function(e) {
-      e.preventDefault();
-      var first_name = document.getElementById('first_name').value;
-      var last_name = document.getElementById('last_name').value;
-      var email = document.getElementById('email').value;
-      var subject = document.getElementById('subject').value;
-      var message = document.getElementById('message').value;
-      var body = 'First Name: ' + first_name + '<br/> Last Name: ' + last_name + '<br/> Email: ' + email + '<br/> Subject: ' + subject + '<br/> Message: ' + message;
-  
-      Email.send({
-        SecureToken: "0869617a-a1ef-42da-981d-f199f8e05cfb",
-        To: 'kevinyegon2015@gmail.com',
-        From: "kevinkipkurui1@gmail.com",
-        Subject: "Contact Message Inquiry from Cedrecs Website",
-        Body: body
-      }).then(function(message) {
-        alert(message);
-      });
-    });
-  });
-  
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent default form submission behavior
+
+  var submitButton = document.getElementById("submitButton");
+  var loadingIcon = document.getElementById("loadingIcon");
+  submitButton.disabled = true; // Disable the button when the form is being submitted
+  loadingIcon.style.display = "inline-block"; // Show the loading icon
+
+  var First_name = document.getElementById("First_name").value;
+  var Last_name = document.getElementById("Last_name").value;
+  var email = document.getElementById("Email").value;
+  var sub = document.getElementById("Subject").value;
+  var mess = document.getElementById("Message").value;
+
+  // URL of the image to be included in the email
+  var imageUrl = "https://i.ibb.co/QDFFCRf/emailbanner.png";
+
+  // Create an HTML template for the email body
+  var body = `
+    <h2>Message from CeDReCS Website Form</h2>
+    <p><strong>Name:</strong> ${First_name} ${Last_name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Subject:</strong> ${sub}</p>
+    <p><strong>Message:</strong></p>
+    <pre style="font-family: 'Arial', sans-serif; font-size: 14px; white-space: pre-wrap; background-color: #f7f7f7; padding: 10px;">
+      <p>${mess}</p>
+    </pre>
+    <p style="text-align: center;">
+      <img src="${imageUrl}" alt="Image" style="width: 100%; height: auto; max-width: 600px;">
+    </p>
+  `;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "kevin.kipkurui@interns.dkut.ac.ke",
+    Password: "929ECBE9F7A98443C45B57C642AAF8DCF0CA",
+    To: 'kevinyegon2015@gmail.com',
+    From: "kevin.kipkurui@interns.dkut.ac.ke",
+    Subject: sub,
+    Body: body,
+    isHTML: true // Set this to true to send the email as HTML
+  }).then(
+    message => {
+      submitButton.disabled = false; // Re-enable the button after the email has been sent
+      loadingIcon.style.display = "none"; // Hide the loading icon
+
+      if (message === 'OK') {
+        swal("Success", "Your Message Successfully Received", "success").then(() => {
+          // Reset the form after successful submission
+          document.getElementById("contactForm").reset();
+        });
+      } else {
+        swal("Error", "Your Message was not Received", "error");
+      }
+    }
+  );
+}
